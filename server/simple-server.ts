5a20ad1d418bdb7e282202fba5ec1dce3f31499c
Marco Barrera Tillería <85660544+marcobarrerati@users.noreply.github.com>
2024-12-10 22:36:16 -0300

    Update simple-server.ts

--- server/simple-server.ts
+++ server/simple-server.ts
@@ -10,11 +10,4 @@ const port = 8000;
 console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 serve(handler, { port });
 
-/**
 
-# Explicación del código
-
-1- serve acepta el handler eque es una función que recibe un Reuest y devuelve un Response
-
-
-*/
